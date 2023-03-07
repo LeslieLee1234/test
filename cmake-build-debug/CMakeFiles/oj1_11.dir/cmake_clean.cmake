f174ad243cdcc5bb0d8caa0e3ab052ee11a8353b
Leslielee1234 <lx1136649944@gmail.com>
2023-03-07 18:13:47 +0800

    text

---
+++ cmake-build-debug/CMakeFiles/oj1_11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oj1_11.dir/main.c.obj"
+  "liboj1_11.dll.a"
+  "oj1_11.exe"
+  "oj1_11.exe.manifest"
+  "oj1_11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/oj1_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
